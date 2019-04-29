8dc6f5219e113336ccb463b20ac041428c676ef1
julesbibo <bibonimana_jules@wheatoncollege.edu>
2019-04-28 22:49:40 -0400

    Gelos Atlas and Fortuna's dataset added

--- backend/mkdir.py
+++ backend/mkdir.py
@@ -3,5 +3,74 @@
 import os, sys
 
 # Path to be created
-path = "CSVdata/Atlas"
-os.mkdir( path, 0755);
+#path0 = "CSVdata/Gelos/hop3"
+#path1 = "CSVdata/Gelos/hop10"
+path2 = "CSVdata/Gelos/hop41"
+#path3 = "CSVdata/Gelos/hop11"
+#os.mkdir( path0, 0755);
+#os.mkdir( path1, 0755);
+#os.mkdir( path3, 0755);
+os.mkdir( path2, 0755);
+
+'''
+path5 = "CSVdata/Gelos/hop13"
+path6 = "CSVdata/Gelos/hop14"
+path7 = "CSVdata/Gelos/hop15"
+path8 = "CSVdata/Gelos/hop16"
+os.mkdir( path5, 0755);
+os.mkdir( path6, 0755);
+os.mkdir( path7, 0755);
+os.mkdir( path8, 0755);
+
+
+path9 = "CSVdata/Gelos/hop17"
+path10 = "CSVdata/Gelos/hop18"
+path11 = "CSVdata/Gelos/hop19"
+path12 = "CSVdata/Gelos/hop22"
+os.mkdir( path9, 0755);
+os.mkdir( path10, 0755);
+os.mkdir( path11, 0755);
+os.mkdir( path12, 0755);
+
+
+path13 = "CSVdata/Gelos/hop23"
+path14 = "CSVdata/Gelos/hop24"
+path15 = "CSVdata/Gelos/hop26"
+path16 = "CSVdata/Gelos/hop29"
+os.mkdir( path13, 0755);
+os.mkdir( path14, 0755);
+os.mkdir( path15, 0755);
+os.mkdir( path16, 0755);
+
+
+path18 = "CSVdata/Gelos/hop30"
+path19 = "CSVdata/Gelos/hop32"
+path20 = "CSVdata/Gelos/hop35"
+path21 = "CSVdata/Gelos/hop37"
+os.mkdir( path18, 0755);
+os.mkdir( path19, 0755);
+os.mkdir( path20, 0755);
+os.mkdir( path21, 0755);
+
+
+path22 = "CSVdata/Gelos/hop38"
+path23 = "CSVdata/Gelos/hop39"
+path24 = "CSVdata/Gelos/hop42"
+path25 = "CSVdata/Gelos/hop43"
+os.mkdir( path22, 0755);
+os.mkdir( path23, 0755);
+os.mkdir( path24, 0755);
+os.mkdir( path25, 0755);
+
+path26 = "CSVdata/Gelos/hop44"
+path27 = "CSVdata/Gelos/hop45"
+path28 = "CSVdata/Gelos/hop47"
+path29 = "CSVdata/Gelos/hop48"
+os.mkdir( path26, 0755);
+os.mkdir( path27, 0755);
+os.mkdir( path28, 0755);
+os.mkdir( path29, 0755);
+
+
+path30 = "CSVdata/Gelos/hop50"
+os.mkdir( path29, 0755);'''
