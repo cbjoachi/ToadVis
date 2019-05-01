55a9b431ed8489734a13c431813e5ffb11b2c47f
julesbibo <bibonimana_jules@wheatoncollege.edu>
2019-05-01 03:51:47 -0400

    Folders for data set

--- backend/mkdir.py
+++ backend/mkdir.py
@@ -3,74 +3,75 @@
 import os, sys
 
 # Path to be created
-#path0 = "CSVdata/Gelos/hop3"
-#path1 = "CSVdata/Gelos/hop10"
-path2 = "CSVdata/Gelos/hop41"
-#path3 = "CSVdata/Gelos/hop11"
+#path0 = "../static/CSVdata/Talos/hop9"
+#path1 = "../static/CSVdata/Talos/hop15"
+#path2 = "../static/CSVdata/Talos/hop16"
+#path3 = "../static/CSVdata/Talos/hop17"
 #os.mkdir( path0, 0755);
 #os.mkdir( path1, 0755);
 #os.mkdir( path3, 0755);
-os.mkdir( path2, 0755);
+#os.mkdir( path2, 0755);
 
-'''
-path5 = "CSVdata/Gelos/hop13"
-path6 = "CSVdata/Gelos/hop14"
-path7 = "CSVdata/Gelos/hop15"
-path8 = "CSVdata/Gelos/hop16"
+'''path5 = "../static/CSVdata/Talos/hop18"
+path6 = "../static/CSVdata/Talos/hop19"
+path7 = "../static/CSVdata/Talos/hop20"
+path8 = "../static/CSVdata/Talos/hop21"
 os.mkdir( path5, 0755);
 os.mkdir( path6, 0755);
 os.mkdir( path7, 0755);
 os.mkdir( path8, 0755);
 
 
-path9 = "CSVdata/Gelos/hop17"
-path10 = "CSVdata/Gelos/hop18"
-path11 = "CSVdata/Gelos/hop19"
-path12 = "CSVdata/Gelos/hop22"
+path9 = "../static/CSVdata/Talos/hop24"
+path10 = "../static/CSVdata/Talos/hop26"
+path11 = "../static/CSVdata/Talos/hop27"
+path12 = "../static/CSVdata/Talos/hop31"
 os.mkdir( path9, 0755);
 os.mkdir( path10, 0755);
 os.mkdir( path11, 0755);
 os.mkdir( path12, 0755);
 
 
-path13 = "CSVdata/Gelos/hop23"
-path14 = "CSVdata/Gelos/hop24"
-path15 = "CSVdata/Gelos/hop26"
-path16 = "CSVdata/Gelos/hop29"
+path13 = "../static/CSVdata/Talos/hop32"
+path14 = "../static/CSVdata/Talos/hop33"
+path15 = "../static/CSVdata/Talos/hop38"
+path16 = "../static/CSVdata/Talos/hop41"
 os.mkdir( path13, 0755);
 os.mkdir( path14, 0755);
 os.mkdir( path15, 0755);
 os.mkdir( path16, 0755);
 
 
-path18 = "CSVdata/Gelos/hop30"
-path19 = "CSVdata/Gelos/hop32"
-path20 = "CSVdata/Gelos/hop35"
-path21 = "CSVdata/Gelos/hop37"
-os.mkdir( path18, 0755);
-os.mkdir( path19, 0755);
-os.mkdir( path20, 0755);
+path18 = "../static/CSVdata/Talos/hop30"
+path19 = "../static/CSVdata/Talos/hop32"
+path20 = "../static/CSVdata/Talos/hop35"
+path21 = "../static/CSVdata/Talos/hop37"
+#os.mkdir( path18, 0755);
+#os.mkdir( path19, 0755);
+#os.mkdir( path20, 0755);
 os.mkdir( path21, 0755);
 
 
-path22 = "CSVdata/Gelos/hop38"
-path23 = "CSVdata/Gelos/hop39"
-path24 = "CSVdata/Gelos/hop42"
-path25 = "CSVdata/Gelos/hop43"
-os.mkdir( path22, 0755);
-os.mkdir( path23, 0755);
+path22 = "../static/CSVdata/Talos/hop38"
+path23 = "../static/CSVdata/Talos/hop41"
+path24 = "../static/CSVdata/Talos/hop42"
+path25 = "../static/CSVdata/Talos/hop44"
+#os.mkdir( path22, 0755);
+#os.mkdir( path23, 0755);
 os.mkdir( path24, 0755);
 os.mkdir( path25, 0755);
 
-path26 = "CSVdata/Gelos/hop44"
-path27 = "CSVdata/Gelos/hop45"
-path28 = "CSVdata/Gelos/hop47"
-path29 = "CSVdata/Gelos/hop48"
+path26 = "../static/CSVdata/Talos/hop45"
+path27 = "../static/CSVdata/Talos/hop46"
+path28 = "../static/CSVdata/Talos/hop50"
+path29 = "../static/CSVdata/Talos/hop52"
 os.mkdir( path26, 0755);
 os.mkdir( path27, 0755);
 os.mkdir( path28, 0755);
-os.mkdir( path29, 0755);
-
-
-path30 = "CSVdata/Gelos/hop50"
 os.mkdir( path29, 0755);'''
+
+
+path30 = "../static/CSVdata/Gelos/hop53"
+path34 = "../static/CSVdata/Gelos/hop54"
+os.mkdir( path30, 0755);
+os.mkdir( path34, 0755);
